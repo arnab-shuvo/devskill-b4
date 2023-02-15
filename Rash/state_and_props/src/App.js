2cec8e32530cbca72964c1b4ba9fdcd6d15f2f1e
isomer04 <isomer008@gmail.com>
2023-02-15 12:09:48 -0500

    Rashed - State and Props Assignment

---
+++ Rash/state_and_props/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Profile from './Profile';
+import NavBar from './NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;

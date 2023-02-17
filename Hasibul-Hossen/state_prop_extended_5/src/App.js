aca4c6de95d8fd3a0be14b0aeb5a95adfdaedd3d
Hasibul3h <hasibul3h1118@gmail.com>
2023-02-17 12:17:20 +0600

    Hasibul - state props extended assignment

--- Hasibul-Hossen/state_prop_extended_5/src/App.js
+++ Hasibul-Hossen/state_prop_extended_5/src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import Product from './product';
-import ProductDetails from './productDetails';
+import { Component } from "react";
+import ProductDetails from "./Component/ProductDetails";
+import Products from "./Component/Products";
 
-function App() {
-  return (
-    <>
-      <Product />
-      <ProductDetails />
-    </>
-  );
+class App extends Component {
+  state = {
+    product: null
+  }
+  selectProduct = (product) => {
+    this.setState(product);
+  }
+  render() {
+    return (
+      <>
+        {this.state.product ? (
+          <ProductDetails selectProduct={this.selectProduct} product={this.state.product} />
+        ) : (
+          <Products selectProduct={this.selectProduct} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;

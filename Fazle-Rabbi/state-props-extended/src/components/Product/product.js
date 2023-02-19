81ec5804ab653d12ea6fda210332acea63b2a007
fazle rabbi <akafazlerabbi@gmail.com>
2023-02-19 23:59:35 +0600

    fix: use map

--- Fazle-Rabbi/state-props-extended/src/components/Product/product.js
+++ Fazle-Rabbi/state-props-extended/src/components/Product/product.js
@@ -41,36 +41,13 @@ export class Product extends Component {
     const productList = (
       <ol>
         <h2>Products</h2>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[0].id)}>
-            {products[0].name} ----------- {products[0].price}
-          </button>
-        </li>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[1].id)}>
-            {products[1].name} ----------- {products[1].price}
-          </button>
-        </li>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[2].id)}>
-            {products[2].name} ----------- {products[2].price}
-          </button>
-        </li>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[3].id)}>
-            {products[3].name} ----------- {products[3].price}
-          </button>
-        </li>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[4].id)}>
-            {products[4].name} ----------- {products[4].price}
-          </button>
-        </li>
-        <li>
-          <button onClick={() => this.setActiveProduct(products[5].id)}>
-            {products[5].name} ----------- {products[5].price}
-          </button>
-        </li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <button onClick={() => this.setActiveProduct(product.id)}>
+              {product.name} ----------- {product.price}
+            </button>
+          </li>
+        ))}
       </ol>
     );
     return (

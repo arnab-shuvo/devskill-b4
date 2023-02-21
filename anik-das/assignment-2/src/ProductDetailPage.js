a73e7c24e2677aa99b775ec7a5ef71e669f45962
anikchandradas <anikchandradas.me@gmail.com>
2023-02-21 13:01:54 +0600

    Updated the folder name

---
+++ anik-das/assignment-2/src/ProductDetailPage.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import ProductPage from "./ProductPage";
+
+// class ProductDetailPage extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <h2>Product Details Page</h2>
+//                 <h3>{this.props.ProductPage}</h3>
+//                 <h3>d</h3>
+//                 <h3>g</h3>
+//             </div>
+//         );
+//     }
+// }
+
+// export default ProductDetailPage;

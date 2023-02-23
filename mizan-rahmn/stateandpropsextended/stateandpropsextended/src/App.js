c7badcaf4823266aada7e7d2bb0bf63c7d3fde82
mizanurrahman13 <mizan.441980@gmail.com>
2023-02-24 01:07:24 +0600

    mizan-rahman => stateandpropsextended

---
+++ mizan-rahmn/stateandpropsextended/stateandpropsextended/src/App.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+import Product from './components/Product';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Product />
+      </>
+    );
+  }
+}
+
+export default App;

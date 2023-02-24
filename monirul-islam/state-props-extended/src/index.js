d741b69f005920155d70a1bcd6aed6dce26a5754
Monirul Islam <monirul.cse14@gmail.com>
2023-02-24 10:07:53 +0600

    monirul: state-props-extended

--- monirul-islam/state-props-extended/src/index.js
+++ monirul-islam/state-props-extended/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Product from './Product';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <Product />
+    
 );
 
 // If you want to start measuring performance in your app, pass a function

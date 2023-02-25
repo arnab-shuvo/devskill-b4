8d8923f2bde9ff1f9057de097c740c165b8ed580
Monirul Islam <monirul.cse14@gmail.com>
2023-02-25 21:05:55 +0600

    monirul-islam:Hooks and Lifecycle

---
+++ monirul-islam/hooks-and-lifecycle/src/ProductDetails.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import {React,useEffect,useState} from 'react';
+import {useLocation} from 'react-router-dom';
+
+function ProductDetails(props){
+    const location=useLocation();
+    const data=location.state;
+
+    const [isLoading,SetLoading]=useState(false);
+    const fetchData=()=>{
+       SetLoading(true);
+       setTimeout(()=>{
+           SetLoading(false);
+       },3500)
+    }
+
+return(
+    
+<div className='page'>
+
+       <p>ID:{" "}{data?data.product.id:"This Product not found "}</p>
+       <p>Name:{" "}{data.product.name}</p>
+       <p>Description:{" "}{data.product.description}</p>
+       <p>Price:{" "}{data.product.price}</p>
+       <p>Category:{" "}{data.product.category}</p>
+</div>
+
+);
+    
+
+}
+export default  ProductDetails;

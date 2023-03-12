39c58ad32f9bdecd382453a6207c692a111d3a55
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2023-03-13 04:28:44 +0600

    Mohammad Ashfaq Islam Asif: Materaial UI - Header part with side menu completed

---
+++ Ashfaq-Islam-Asif/material-ui/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Box } from "@mui/material";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
+
+export default App;
---
+++ Ashfaq-Islam-Asif/material-ui/src/Components/SideMenu.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { ContactPage, Home, Info } from "@mui/icons-material";
+
+const SideMenu = ({ openDrawer, onClose }) => {
+  return (
+    <Drawer anchor="right" open={openDrawer} onClose={onClose}>
+      <Box
+        sx={{
+          width: 250,
+          backgroundColor: "rgba(0,0,0, 0.7)",
+          height: "100%",
+        }}
+        role="presentation"
+      >
+        <List>
+          {["Home", "About", "Contact"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton disableGutters>
+                <ListItemIcon sx={{ pl: 2, mr: -2 }}>
+                  {text == "Home" ? (
+                    <Home sx={{ color: "white" }} />
+                  ) : text == "About" ? (
+                    <Info sx={{ color: "white" }} />
+                  ) : (
+                    <ContactPage sx={{ color: "white" }} />
+                  )}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ color: "white" }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default SideMenu;

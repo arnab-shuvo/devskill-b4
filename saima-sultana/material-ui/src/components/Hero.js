d402ef0de6aa8342b95f176524b9e04f27da5466
SaimaMeem <saimasultana1320@gmail.com>
2023-03-13 21:33:06 +0600

    update hero section

--- saima-sultana/material-ui/src/components/Hero.js
+++ saima-sultana/material-ui/src/components/Hero.js
@@ -1,5 +1,11 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
+import { Icon } from '@mui/material';  
+const buttonStyle = {
+    textTransform: 'capitalize',
+    fontWeight: 'bold',
+};
+// backgroundColor: 'hsl(217, 57%, 51%)',
 const Hero = () => {
     return (
         //hero section
@@ -9,23 +15,8 @@ const Hero = () => {
                 <h1 className="text-xl sm:text-5xl font-semibold sm:mb-2">Keiths Williams</h1>
                 <h4 className="text-sm sm:text-base uppercase tracking-[0.3rem] sm:tracking-[0.5rem]">Senior Developer</h4>
                 <div className="hidden sm:flex flex-wrap my-10 space-x-6">
-                    <button type="button"
-                        className="flex items-center rounded border-2 border-primary bg-white px-7 pt-3 pb-2.5 font-bold leading-normal text-primary shadow-[0_4px_9px_-4px_#3b71ca] transition duration-500 ease-in-out hover:bg-primary hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] hover:text-white focus:bg-primary focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 focus:text-white active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                        data-te-ripple-init>
-                        View Portifolio
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                            stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                        </svg>
-                    </button>
-                    <button type="button"
-                        className="flex items-center rounded bg-primary px-7 pt-3 pb-2.5 font-bold leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-500 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                        data-te-ripple-init>
-                        Download Resume <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                        </svg>
-                    </button>
+                    <Button variant="outlined" size="large" style={buttonStyle}>View Portfolio<Icon sx={{ fontSize: 20 }}>arrow_forward_ios</Icon></Button>
+                    <Button variant="contained" size="large"  style={buttonStyle}>Download Resume<Icon sx={{ fontSize: 20 }}>arrow_forward_ios</Icon></Button>
                 </div>
             </div>
         </section>

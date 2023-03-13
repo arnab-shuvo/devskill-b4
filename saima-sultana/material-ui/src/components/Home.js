b5be1226df82c4ccd465aaf3d3a16781ca8d9e9b
SaimaMeem <saimasultana1320@gmail.com>
2023-03-13 21:54:49 +0600

    add about section

--- saima-sultana/material-ui/src/components/Home.js
+++ saima-sultana/material-ui/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AboutMe from './AboutMe';
 import Hero from './Hero';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <AboutMe/>
         </div>
     );
 };

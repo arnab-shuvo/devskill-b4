31fc2cb74917d04f376b3fb5f29ee6a31a985e76
isomer04 <isomer008@gmail.com>
2023-03-27 12:25:05 -0400

    Rashed -API and Router are  not finished yet

---
+++ Rash/api_and_router/src/App.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import NotFound from "./pages/404";
+
+import ProductDetail from "./pages/product-detail";
+import AddProductForm from "./pages/create-product";
+
+function App() {
+  const [products, setProduct] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.escuelajs.co/api/v1/products?offset=0&limit=30")
+      .then((res) => {
+        setProduct(res.data);
+      });
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product-detail/:id" element={<ProductDetail />} />
+      <Route path="/add-product" component={AddProductForm} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;

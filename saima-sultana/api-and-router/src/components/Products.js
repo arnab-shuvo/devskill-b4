87dbb6bf710203edafe82b0f6bf8b585581c3241
SaimaMeem <saima.sultana1320@gmail.com>
2023-03-29 02:17:01 +0600

    SaimaMeem: add loader in home page

--- saima-sultana/api-and-router/src/components/Products.js
+++ saima-sultana/api-and-router/src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from './Product';
+import Loader from '../shared/Loader';
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
@@ -9,8 +10,11 @@ const Products = () => {
         })
     }, []);
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 m-10 justify-center'>
-            {products.length > 0 ? products.map(product => <Product key={product.id} product={product}></Product>) : <p>Loading...</p>}
+        <div>
+            {products.length > 0 ? <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 m-10 justify-center'>
+                {products.map(product => <Product key={product.id} product={product}></Product>)}
+            </div>
+                : <Loader />}
         </div>
     );
 };

d24eb32b0006260de7815c482c7c01599b3e98dc
fazle rabbi <akafazlerabbi@gmail.com>
2023-04-01 14:00:58 +0600

    fix: change name of productSlice

--- Fazle-Rabbi/redux/src/reducer/productsReducer.js
+++ Fazle-Rabbi/redux/src/reducer/productsReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const productsSlice = createSlice({
-  name: "example",
+  name: "products",
   initialState: {
     products: [],
     singleProduct: {

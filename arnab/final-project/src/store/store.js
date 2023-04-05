ccd8a0effa5ac7a0847db6170e1a4269dc43f494
Arnab Dhar <arnabdhar@192.168.1.101>
2023-04-05 18:47:38 +0100

    Final Project initiated

---
+++ arnab/final-project/src/store/store.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./reducers/userSlice";
+
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "user-info",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);

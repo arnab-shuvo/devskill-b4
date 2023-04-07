2123f10ca18ba0e5a6b5f2f46c3382184e1a3e39
hasibul3h <hasibul3h1118@gmail.com>
2023-04-07 15:47:03 +0600

    Hasibul - final project instalation

---
+++ final-project/hasibul-hossen/gardomia/src/components/home/homeMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeMain = () => {
+    return (
+        <>
+            This is the main page
+        </>
+    );
+};
+
+export default HomeMain;
---
+++ final-project/hasibul-hossen/gardomia/src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginMain from '../../components/login/loginMain';
+
+const index = () => {
+    return (
+        <>
+            <LoginMain />
+        </>
+    );
+};
+
+export default index;

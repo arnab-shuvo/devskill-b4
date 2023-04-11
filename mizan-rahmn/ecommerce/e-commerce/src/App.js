e230a653caad5a566db0ac09d5da84bcc3a24494
mizanurrahman13 <mizan.441980@gmail.com>
2023-04-11 08:47:03 +0600

    Added Registration Component

--- mizan-rahmn/ecommerce/e-commerce/src/App.js
+++ mizan-rahmn/ecommerce/e-commerce/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Route index path="/" element={<>Home</>} />
       <Route>
         <Route index path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Route>
       <Route path="*" element={<>404</>} />
     </Routes>

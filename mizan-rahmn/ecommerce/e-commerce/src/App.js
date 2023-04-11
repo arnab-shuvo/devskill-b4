211f631913f01d9084b9d021ff5b86e01badc579
mizanurrahman13 <mizan.441980@gmail.com>
2023-04-11 07:50:57 +0600

    Added Login Component

--- mizan-rahmn/ecommerce/e-commerce/src/App.js
+++ mizan-rahmn/ecommerce/e-commerce/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route index path="/" element={<>Home</>} />
+      <Route>
+        <Route index path="/login" element={<Login />} />
+      </Route>
+      <Route path="*" element={<>404</>} />
+    </Routes>
   );
 }
 
---
+++ mizan-rahmn/ecommerce/e-commerce/src/pages/login/index.js
@@ -0,0 +1,76 @@
+import { LoginContainer } from "./styled";
+import Card from "@mui/material/Card";
+import { CardContent, CardHeader } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { useForm } from "react-hook-form";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/system";
+import Alert from "@mui/material/Alert";
+import { userLogin } from "../../store/actions/userAction";
+import { useDispatch } from "react-redux";
+import Link from "@mui/material/Link";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    dispatch(userLogin(data));
+  };
+  return (
+    <LoginContainer container justifyContent={"center"} alignItems="center">
+      <Card sx={{ width: 500, padding: "60px" }}>
+        <CardHeader style={{ textAlign: "center" }} title="Login" />
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              style={{ margin: "0 0 20px 0" }}
+              id="standard-basic"
+              label="Email *"
+              placeholder="Please Enter Email"
+              fullWidth
+              variant="outlined"
+              type="email"
+              error={errors.email}
+              {...register("email", {
+                required: true,
+              })}
+            />
+            {errors.email && <Alert severity="error">Email Required</Alert>}
+            <TextField
+              style={{ margin: "10px 0" }}
+              id="standard-basic"
+              label="Password *"
+              placeholder="Please Enter Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              error={errors.password}
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <Alert severity="error">Password Required</Alert>
+            )}
+
+            <Box sx={{ padding: "20px 0px 0 0" }}>
+              <Button type="submit" variant="contained">
+                Login
+              </Button>
+            </Box>
+            <Box sx={{ padding: "20px 0px 0 0" }}>
+              <p>
+                Not Registered? Please <Link href="/signup">Sign Up</Link>
+              </p>
+            </Box>
+          </form>
+        </CardContent>
+      </Card>
+    </LoginContainer>
+  );
+};
+
+export default Login;

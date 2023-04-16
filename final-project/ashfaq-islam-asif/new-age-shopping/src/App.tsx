6db0ad144e4687c92469359dd2d864e62cbf7ff8
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2023-04-16 23:33:07 +0600

    Md Ashfaq Islam Asif - Final Project: Material UI added

--- final-project/ashfaq-islam-asif/new-age-shopping/src/App.tsx
+++ final-project/ashfaq-islam-asif/new-age-shopping/src/App.tsx
@@ -1,5 +1,7 @@
+import { Typography } from '@mui/material';
+
 const App = () => {
-    return <>New Age Shopping</>;
+    return <Typography fontSize={14}>New Age Shopping</Typography>;
 };
 
 export default App;
